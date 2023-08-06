b0b5c4a00544064c045bc648e48485ff4f8e6ec0
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2023-08-06 21:49:19 +0300

    feat: "react hooks"

---
+++ counter-app/README.md
@@ -0,0 +1,9 @@
+# Key Takeaways
+
+I discovered that there are specialized functions called hooks designed for this purpose.\
+These hooks include useState, useEffect, and useRef, which empower developers to seamlessly manage states and\ the lifecycle of their applications.\
+
+I used useState to dynamically update the counter variable.\
+useEffect to automatically adjust the document title whenever the counter underwent a change.\
+useRef to capture the button element and dynamically modify its background color upon being clicked.\
+
---
+++ counter-app/src/App.js
@@ -0,0 +1,40 @@
+import { useEffect, useRef, useState } from 'react';
+import './App.css';
+
+function App() {
+  //counter state
+  const [counter, setCounter] = useState(0);
+  
+  //Ref for increase and decrease buttons, used to grab the buttons and update their background colors when button is clicked   
+  const increaseButton = useRef(null);
+  const decreaseButton = useRef(null);
+
+  //Effect hook to update the document title when counter is changed
+  useEffect(() => {
+    document.title = counter;
+  }, [counter]);
+
+  return (
+    <div className="App">
+      <p>{counter}</p>
+      <div className="buttons"> 
+
+        {/* update counter when button is clicked by calling setCounter, and change the color of the buttons */}
+        <button ref = {increaseButton} onClick={() => {
+          setCounter(counter + 1);
+          increaseButton.current.style.backgroundColor = "green";
+          decreaseButton.current.style.backgroundColor = "lightblue";
+        }}>Increase</button>
+
+  <button ref = {decreaseButton} onClick={() => {
+          setCounter(counter - 1);
+          decreaseButton.current.style.backgroundColor = "green";
+          increaseButton.current.style.backgroundColor = "lightblue";
+        }}>Decrease</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
