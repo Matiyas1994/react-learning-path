d931ad9a3b7306e2b23acc78a21c5bc78a415850
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2023-08-07 08:48:41 +0300

    feat:"finalized"

---
+++ contact-management-app/src/index.js
@@ -0,0 +1,52 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import EditContact, {action as editAction} from "./routes/edit";
+import {action as deleteAction} from "./routes/delete";
+import Intro from "./routes/intro";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage/>,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      { 
+        index: true, element: <Intro/> 
+      },
+
+      {
+        path: "contacts/:contactId",
+        element: <Contact/>,
+        loader: contactLoader
+      },
+
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact/>,
+        loader: contactLoader,
+        action: editAction
+      },
+
+      {
+        path: "contacts/:contactId/destroy",
+        action: deleteAction
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
